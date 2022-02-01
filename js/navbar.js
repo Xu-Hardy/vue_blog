1c12972a982168a0997db47a739dfc2aa877d27c
mengze-han <UFO.1997@QQ.COM>
2022-02-01 10:17:30 +0000

    Deploying to gh-pages from  @ f618cead2336289057a3688b47ebd287ff905762 🚀

---
+++ js/navbar.js
@@ -0,0 +1,56 @@
+module.exports = {
+    nav: [
+          { text: 'Blog', link:'https://mengze-han.github.io/'},
+          { text: 'Home', link: '/' },
+          { text: 'About', link: '/about'},
+          {
+            text: '技术',
+            items: [
+              { 
+                text: '开发', items: [
+                { text: 'python', link: '/python/' },
+                { text: 'c++', link: '/linux/' },
+                { text: 'AI', link: '/python/' },
+                ] 
+              },
+                { text: '运维', items: [
+                { text: 'Linux', link: '/python/' },
+                { text: 'linux', link: '/linux/' },
+                { text: 'AI', link: '/python/' },
+                ] 
+              },
+              { text: 'AI', items: [
+                { text: '深度学习', link: '/python/' },
+                { text: 'linux', link: '/linux/' },
+                { text: 'AI', link: '/python/' },
+                ] 
+            },
+            { text: '云计算', items: [
+              { text: 'AWS', link: '/python/' },
+              { text: 'Azure', link: '/linux/' },
+              { text: 'GCP', link: '/python/' },
+              { text: 'OTHER', link: '/python/' },
+              { text: '私有云',link: '/python/' }
+              ] 
+          },
+            { text: '基本功', items: [
+              { text: '数据结构', link: '/data_structure/' },
+              { text: '计算机组成原理', link: '/linux/' },
+              { text: '计算机网络', link: '/python/' },
+              ] 
+          }
+            ]
+          },
+            { text: '随笔', items: [
+            { text: 'Essay', link: '/Essay/' },
+            { text: 'linux', link: '/linux/' },
+            { text: 'AI', link: '/python/' },
+            ] 
+          },
+          { text: 'GEEK', items: [
+            { text: '装机', link: '/computer/' },
+            { text: 'DIY', link: '/diy/' },
+            { text: 'Latex', link: '/python/' },
+          ] },
+        ],
+}
---
+++ js/sidebar.js
@@ -0,0 +1,15 @@
+module.exports = {
+    sidebar: {
+        '/Python/': [
+          '',     /* /foo/ */
+          '1',  /* /foo/one.html */
+          '2'   /* /foo/two.html */
+        ],
+       // fallback
+        '/': [
+          '',        /* / */
+          // 'contact', /* /contact.html */
+          'about'    /* /about.html */
+        ]
+      }
+}
